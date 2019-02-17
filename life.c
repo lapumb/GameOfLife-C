96f0f0be34509e8b56b133d6432548224410243a
Blake Lapum <lapumb@mail.gvsu.edu>
2019-02-17 00:52:34 -0500

    bug fixes

--- life.c
+++ life.c
@@ -7,26 +7,29 @@
  * get_grid creates new memory for a "grid".
  * x is the height and y is the width.
  */
-char** get_grid(int x, int y) {
+char **get_grid(int x, int y)
+{
 	// an array size x, each each index points to an array size y
 	// ex; x = 2, y = 3 (char)
 	// x --> y y y
 	// x --> y y y
 
 	//checking for errors
-	if(x > 30 || y > 30 || x < 3 || y < 3) {
-		printf("The size of your grid must be between 3 and 30. Please retry. \n"); 
-		exit(1); 
+	if (x > 30 || y > 30 || x < 3 || y < 3)
+	{
+		printf("The size of your grid must be between 3 and 30. Please retry. \n");
+		exit(1);
 	}
 
 	//allocating the num rows
-	char **c = (char **)malloc(x*sizeof(char));
-	
+	char **c = (char **)malloc(x * sizeof(char));
+
 	//adding columns to the rows
-	for(int i = 0; i < x; i++) {
-		*(c+i) = (char *)malloc(y*sizeof(char));
+	for (int i = 0; i < x; i++)
+	{
+		*(c + i) = (char *)malloc(y * sizeof(char));
 	}
-	
+
 	return c;
 }
 
@@ -35,20 +38,25 @@ char** get_grid(int x, int y) {
  * by y width grid stored at the location
  * provided by grid
  */
-void print_grid(int x, int y, char** grid) {
-	for(int i = 0; i < x; i++) {
-		for(int j = 0; j < y; j++) {
-			if(*(*(grid+i)+j) == 0) {
-				*(*(grid+i)+j) = '.';
+void print_grid(int x, int y, char **grid)
+{
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+			if (*(*(grid + i) + j) == 0)
+			{
+				*(*(grid + i) + j) = '.';
 			}
-			else if(*(*(grid+i)+j) == 1) {
-				*(*(grid+i)+j) = 'X'; 
+			else if (*(*(grid + i) + j) == 1)
+			{
+				*(*(grid + i) + j) = 'X';
 			}
-			printf("%c ", (*(*(grid+i)+j)));
+			printf("%c ", (*(*(grid + i) + j)));
 		}
-		printf("\n"); 
+		printf("\n");
 	}
-	printf("\n"); 
+	printf("\n");
 }
 
 /*
@@ -56,7 +64,8 @@ void print_grid(int x, int y, char** grid) {
  * according to Conway's rules.  A new grid
  * is returned.
  */
-char** mutate(int x, int y, char** grid) {
+char **mutate(int x, int y, char **grid)
+{
 	/*
 	1. A live cell with less than two live neighbors dies
 	2. A live cell with two or three live neighbors lives
@@ -64,35 +73,45 @@ char** mutate(int x, int y, char** grid) {
 	4. A dead cell with three live neighbors becomes live
 	*/
 	char **temp = get_grid(x, y);
-	for(int m = 0; m < x; m++) {
-		for(int n = 0; n < y; n++) {
-			temp[m][n] = grid[m][n]; 
+	for (int m = 0; m < x; m++)
+	{
+		for (int n = 0; n < y; n++)
+		{
+			temp[m][n] = grid[m][n];
 		}
-	} 
+	}
 
-	int numNeighbors = 0; 
-	for(int i = 0; i < x; i++) {
-		for(int j = 0; j < y; j++) {
-			numNeighbors = get_neighbors(i, j, x, y, temp); 
-			if(*(*(grid+i)+j) == 'X') {
-				if(numNeighbors < 2) {
+	int numNeighbors = 0;
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+			numNeighbors = get_neighbors(i, j, x, y, temp);
+			if (*(*(grid + i) + j) == 'X')
+			{
+				if (numNeighbors < 2)
+				{
 					//dying
-					*(*(grid+i)+j) = '.';
+					*(*(grid + i) + j) = '.';
 				}
-				else if(numNeighbors == 2 || numNeighbors == 3) {
+				else if (numNeighbors == 2 || numNeighbors == 3)
+				{
 					//lives
-					*(*(grid+i)+j) = 'X';
+					*(*(grid + i) + j) = 'X';
 				}
-				else if(numNeighbors > 3) {
+				else if (numNeighbors > 3)
+				{
 					//dying
-					*(*(grid+i)+j) = '.';
+					*(*(grid + i) + j) = '.';
 				}
 				//printf("%d \n", numNeighbors);
 			}
-			else{
-				if(numNeighbors == 3) {
+			else
+			{
+				if (numNeighbors == 3)
+				{
 					//live
-					*(*(grid+i)+j) = 'X';
+					*(*(grid + i) + j) = 'X';
 				}
 			}
 		}
@@ -105,69 +124,87 @@ char** mutate(int x, int y, char** grid) {
 /* get_neighbors is a helper method that returns
  * the number of live neighbors a cell has. i and j are the coords
  */
-int get_neighbors(int i, int j, int x, int y, char** grid) {
-	int count = 0; 
-	
+int get_neighbors(int i, int j, int x, int y, char **grid)
+{
+	int count = 0;
+
 	//up/left
-	if(i > 0 && j > 0 && i < x && j < y) { //making sure i and j are in bounds
-		if(*(*(grid+i-1)+j-1) == 'X') {
-			count++; 
+	if (i > 0 && j > 0 && i < x && j < y)
+	{ //making sure i and j are in bounds
+		if (*(*(grid + i - 1) + j - 1) == 'X')
+		{
+			count++;
 		}
 	}
 
 	//up
-	if(i > 0 && j >= 0 && i <= x && j <= y) {
-		if(*(*(grid+i-1)+j) == 'X') {
-			count++; 
+	if (i > 0 && j >= 0 && i <= x && j <= y)
+	{
+		if (*(*(grid + i - 1) + j) == 'X')
+		{
+			count++;
 		}
 	}
 
 	//up/right
-	if(i > 0 && j >= 0 && i < x && j < (y-1)) { 
-		if(*(*(grid+i-1)+j+1) == 'X') {
-			count++; 
+	if (i > 0 && j >= 0 && i < x && j < (y - 1))
+	{
+		if (*(*(grid + i - 1) + j + 1) == 'X')
+		{
+			count++;
 		}
 	}
 
 	//left
-	if(i >= 0 && j > 0 && i < x && j < y) { 
-		if(*(*(grid+i)+j-1) == 'X') {
-			count++; 
+	if (i >= 0 && j > 0 && i < x && j < y)
+	{
+		if (*(*(grid + i) + j - 1) == 'X')
+		{
+			count++;
 		}
 	}
 
 	//right
-	if(i >= 0 && j >= 0 && i < x && j < (y-1)) { 
-		if(*(*(grid+i)+j+1) == 'X') {
-			count++; 
+	if (i >= 0 && j >= 0 && i < x && j < (y - 1))
+	{
+		if (*(*(grid + i) + j + 1) == 'X')
+		{
+			count++;
 		}
 	}
 
 	//down/left
-	if(i >= 0 && j > 0 && i < (x-1) && j < y) { 
-		if(*(*(grid+i+1)+j-1) == 'X') {
-			count++; 
+	if (i >= 0 && j > 0 && i < (x - 1) && j < y)
+	{
+		if (*(*(grid + i + 1) + j - 1) == 'X')
+		{
+			count++;
 		}
 	}
 
 	//down
-	if(i >= 0 && j >= 0 && i < (x-1) && j < y) { 
-		if(*(*(grid+i+1)+j) == 'X') {
-			count++; 
+	if (i >= 0 && j >= 0 && i < (x - 1) && j < y)
+	{
+		if (*(*(grid + i + 1) + j) == 'X')
+		{
+			count++;
 		}
 	}
 
 	//down/right
-	if(i >= 0 && j >= 0 && i < (x-1) && j < (y-1)) {
-		if(*(*(grid+i+1)+j+1) == 'X') {
-			count++; 
+	if (i >= 0 && j >= 0 && i < (x - 1) && j < (y - 1))
+	{
+		if (*(*(grid + i + 1) + j + 1) == 'X')
+		{
+			count++;
 		}
 	}
 
 	//if i,j is out of bounds
-	if(i > x || j > y || i < 0 || j < 0) {
-		return -1; 
+	if (i > x || j > y || i < 0 || j < 0)
+	{
+		return -1;
 	}
-	
+
 	return count;
 }
